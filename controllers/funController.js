da01cf390adbe6cc28b35a70f9527a5ea679bc59
Belayet Hossain <soykethandymama@gmail.com>
2019-09-22 18:49:43 +0600

    create fun function for compare

--- controllers/funController.js
+++ controllers/funController.js
@@ -1,7 +1,15 @@
 const Stringstore   	= require('../models/stringstore');
 
 exports.addString = async (req, res) => {
-	const data = await Stringstore.create(req.body);
+	let data;
+	let stra = req.body.strA.split('');
+	let strb = req.body.strB.split('');
+	// call fun function
+	const funFunc = fun(stra, strb);
+	console.log(stra, strb);
+	if (funFunc) {
+		data = await Stringstore.create(req.body);
+	}
 
 	if (!data) {
 		res.status(404).json({
@@ -10,10 +18,32 @@ exports.addString = async (req, res) => {
 		});
 		return;
 	}
-
+	// send response
 	res.status(200).json({
 		'content-type': req.headers["content-type"],
 		message: "String store successfully",
 		data,
 	});
 };
+
+// fun function for compare between strA & strB
+function fun(stra, strb) {
+	if (strb.length <= 0){
+		stra = [];
+		// strB = [' '];
+	}else {
+		const strlen = stra.length;
+		for (let i = 0;i<strlen; i++) {
+			if (strb[i] !== stra[i]) {
+				stra.splice(i,1);
+			}
+		}
+		stra = stra.join('');
+		strb = strb.join('');
+		console.log(stra, strb);
+	}
+
+	if (stra === strb) {
+		return true;
+	}
+}
